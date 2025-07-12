27cbbdfba35586cd3330a3dbb4fb0159603a3b9e
Jeet <jeetnath2110@gmail.com>
2025-07-12 16:48:24 +0530

    About section continue.

--- resources/js/Components/About.tsx
+++ resources/js/Components/About.tsx
@@ -7,8 +7,10 @@ const About = () => {
                 <img src="/assets/images/top-about.jpg" alt="back-about" className='h-[450px] w-5/6 object-cover -mt-[15%] rounded shadow-md' loading='lazy' />
                 <img src="/assets/images/trucks-image.jpg" alt="back-about" className='h-48 w-48 object-cover rounded shadow-md absolute -bottom-8 right-5' loading='lazy' />
             </div>
-            <div className='w-1/2 relative'>
-
+            <div className='w-1/2 relative pe-14'>
+                <h6 className='uppercase text-base font-medium'>about company</h6>
+                <h2 className='text-4xl/[50px] font-black capitalize my-3'>global logistics solution provider 1996</h2>
+                <p className='capitalize text-lg text-red-700 font-medium'>simplify your thought and logistics with a personal approach.</p>
             </div>
         </section>
     )
